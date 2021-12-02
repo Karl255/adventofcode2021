115422d03e66d6d28de240be3f5083f782c12b6c
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-12-02 09:46:47 +0100

    Day 2: added solution in C

---
+++ day-2/part-1-c/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+struct {
+	int x;
+	int z;
+} submarine;
+
+int main() {
+	FILE* input;
+	char c;
+	int t;
+	
+	input = fopen("../input.txt", "r");
+	
+	while (1) {
+		c = fgetc(input);
+		
+		if (c == 'f') {
+			fseek(input, 7, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.x += t;
+		} else if (c == 'u') {
+			fseek(input, 2, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.z -= t;
+		} else if (c == 'd') {
+			fseek(input, 4, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.z += t;
+		} else {
+			break;
+		}
+		
+		fseek(input, 1, SEEK_CUR);
+	}
+	
+	fclose(input);
+	printf("%d\n", submarine.x * submarine.z);
+	
+	return 0;
+}
---
+++ day-2/part-2-c/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+struct {
+	int x;
+	int z;
+	int aim;
+} submarine;
+
+int main() {
+	FILE* input;
+	char c;
+	int t;
+	
+	input = fopen("../input.txt", "r");
+	
+	while (1) {
+		c = fgetc(input);
+		
+		if (c == 'f') {
+			fseek(input, 7, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.x += t;
+			submarine.z += t * submarine.aim;
+		} else if (c == 'u') {
+			fseek(input, 2, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.aim -= t;
+		} else if (c == 'd') {
+			fseek(input, 4, SEEK_CUR);
+			fscanf(input, "%d", &t);
+			submarine.aim += t;
+		} else {
+			break;
+		}
+		
+		fseek(input, 1, SEEK_CUR);
+	}
+	
+	fclose(input);
+	printf("%d\n", submarine.x * submarine.z);
+	
+	return 0;
+}
