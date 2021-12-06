54f524133d5643a4c7cbb1934d31c8584c0936d3
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-12-06 15:21:29 +0100

    Day 6: added solution in C

---
+++ day-6/part-2.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+	size_t t, i, j, ages[9] = { 0 }, sum = 0;
+	FILE* input = fopen("input.txt", "r");
+	
+	do {
+		fscanf(input, "%zd", &t);
+		
+		ages[t]++;
+	} while (fgetc(input) == ',');
+	
+	fclose(input);
+	
+	for (i = 0; i < 256; i++) {
+		t = ages[0];
+		
+		for (j = 1; j < 9; j++) {
+			ages[j - 1] = ages[j];
+		}
+		
+		ages[8] = t;
+		ages[6] += t;
+	}
+	
+	for (i = 0; i < 9; i++) {
+		sum += ages[i];
+	}
+	
+	printf("%zd\n", sum);
+	
+	return 0;
+}
