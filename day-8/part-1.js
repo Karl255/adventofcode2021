0b2651b571ee67f0342d759624c209256ebe45d2
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-12-08 14:06:08 +0100

    Day 8: shortened part 1 solution in JS

--- day-8/part-1.js
+++ day-8/part-1.js
@@ -1,1 +1,1 @@
-console.log(require("fs").readFileSync("input.txt", "ascii").slice(0, -1).split("\n").map(l=>l.split(" | ")[1].split(" ").filter(x=>x.length-5&6).length).reduce((x, y)=>x+y));
+console.log(require("fs").readFileSync("input.txt","ascii").trim().split("\n").map(l=>l.split(" | ")[1].split(" ").filter(x=>x.length-5&6).length).reduce((x, y)=>x+y));
