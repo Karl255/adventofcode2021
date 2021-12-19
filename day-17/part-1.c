131bbbe1fc14fd21e376972c48f4111cca40bffb
Karl255 <49595438+Karl255@users.noreply.github.com>
2021-12-19 14:57:21 +0100

    Day 17: added solution in C

---
+++ day-17/part-1.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int simulate(int, int, int, int, int, int);
+
+int main() {
+	int x, y, maxY = 0, target_minX, target_maxX, target_minY, target_maxY, t;
+	
+	FILE* input = fopen("input.txt", "r");
+	fscanf(input, "target area: x=%d..%d, y=%d..%d", &target_minX, &target_maxX, &target_minY, &target_maxY);
+	fclose(input);
+	
+	for (y = 500; y > target_minY; y--) {
+		for (x = 1; x <= target_maxX; x++) {
+			maxY = simulate(x, y, target_minX, target_maxX, target_minY, target_maxY);
+			
+			if (maxY > 0) {
+				goto end;
+			}
+		}
+	}
+	end:
+	
+	printf("%d\n", maxY);
+	
+	return 0;
+}
+
+int simulate(int v_x, int v_y, int t_minX, int t_maxX, int t_minY, int t_maxY) {
+	int x = 0, y = 0, maxY = 0;
+	
+	while ((x < t_minX || y > t_maxY) && y >= t_minY) {
+		x += v_x;
+		y += v_y;
+		
+		if (y > maxY) {
+			maxY = y;
+		}
+		
+		if (v_x > 0) {
+			v_x--;
+		}
+		
+		v_y--;
+	}
+	
+	return x <= t_maxX && y >= t_minY ? maxY : -1;
+}
